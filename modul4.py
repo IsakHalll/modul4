473b502e5bf72312d69dc9d320e7e047d7b4589b
IsakHalll <isak.akerberg@elev.ga.ntig.se>
2023-09-05 09:58:22 +0200

    aaa

---
+++ modul4.py
@@ -0,0 +1,18 @@
+import os
+os.system('cls')
+"""
+min_length=1.4
+length = float(input("din längd i meter: "))
+if length >= min_length:
+    print ("du får åka")
+elif length < min_length:
+    print ("Kortis du får inte åka")
+"""
+
+print ("välkommen")
+name = str(input("Namn: "))
+age = int(input("Ålder: "))
+try:
+    print(x)
+except:
+    print(skriv i heltal)
